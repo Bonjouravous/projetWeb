099738168bc48008b5b8adf6a8cff206e5df4e93
Ambroise Renaud <ambroise.renaud@gmail.com>
2018-06-11 20:27:04 +0200

    articles style

---
+++ articleStyle.php
@@ -0,0 +1,103 @@
+ <?php include('header.php'); ?>
+ <div class="container">
+ 	<div class="card">
+ 		<img class="card-img-top" src="images/700x400.png" alt="Card image cap">
+ 		<div class="card-body">
+ 			<h5 class="card-title">Titre du Lieu</h5>
+ 			<p class="card-text"><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In earum voluptates non. Explicabo sunt, inventore rem. Odit aliquid beatae ut, dolores est ex maiores odio, eaque, eum, molestias dolor fugiat!</span>
+ 				<span>Saepe dignissimos illum dolorem! Debitis laborum, quam quaerat dolorem non temporibus dolores, pariatur at commodi praesentium. Quibusdam animi pariatur aliquam ipsa necessitatibus, repudiandae a qui, earum autem praesentium dolorem magnam!</span>
+ 				<span>Fuga nisi, eum provident eveniet ullam, suscipit error? Facilis nesciunt sit obcaecati nisi enim praesentium architecto possimus minima doloremque laboriosam laudantium et repellat impedit quasi cum, voluptatem iusto illo, esse.</span>
+ 				<span>Deserunt, soluta minus cupiditate sunt necessitatibus maxime nisi obcaecati fuga eius numquam! Ut reprehenderit esse dolorum dolores ipsum tempore illum rem vero, iste necessitatibus deleniti amet ad perferendis, deserunt nam!</span>
+ 				<span>Natus voluptatem tempora eos quod accusantium ratione incidunt, maiores, ipsa aliquid quo cupiditate fugit quibusdam non id quos. Quidem repudiandae, cupiditate eum explicabo doloribus sunt reprehenderit deserunt blanditiis totam incidunt.</span></p>
+ 				<a href="#" class="btn btn-success">1024 <i class="fa fa-thumbs-up" style="font-size: 13px; padding:0;"></i></a>
+ 				<a href="#" class="btn btn-outline-success">Modifier</a>
+ 				<div class="text-right text-muted">Dernière modification le 11/06/2018</div>
+ 				<div class="text-right text-muted">Nom de l'auteur</div>
+ 			</div>
+ 		</div>
+ 		<div id="commentaires">
+ 			<div class="titre">
+ 				<label>Commentaires</label>
+ 			</div>
+ 			<div class="suggestion">
+
+ 				<p class="descriptionCommentaire">Partagez votre retour d'expérience !</p>
+ 			</div>
+ 			<div class="actionBox">
+ 				<ul class="commentList">
+ 					<li>
+ 						<div class="commenterImage">
+ 							<img src="https://dummyimage.com/50x50/d3d3d3/fff" />
+ 						</div>
+ 						<div class="commentText">
+ 							<p class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio neque voluptas expedita obcaecati blanditiis distinctio numquam libero est placeat laborum repellendus consequatur, odit veniam corporis rerum iure commodi minus sunt.</p> <span class="date sub-text">Le 11/06/2018</span>
+
+ 						</div>
+ 					</li>
+ 					<li>
+ 						<div class="commenterImage">
+ 							<img src="https://dummyimage.com/50x50/d3d3d3/fff" />
+ 						</div>
+ 						<div class="commentText">
+ 							<p class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio neque voluptas expedita obcaecati blanditiis distinctio numquam libero est placeat laborum repellendus consequatur, odit veniam corporis rerum iure commodi minus sunt.</p> <span class="date sub-text">Le 11/06/2018</span>
+
+ 						</div>
+ 					</li>					<li>
+ 						<div class="commenterImage">
+ 							<img src="https://dummyimage.com/50x50/d3d3d3/fff" />
+ 						</div>
+ 						<div class="commentText">
+ 							<p class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio neque voluptas expedita obcaecati blanditiis distinctio numquam libero est placeat laborum repellendus consequatur, odit veniam corporis rerum iure commodi minus sunt.</p> <span class="date sub-text">Le 11/06/2018</span>
+
+ 						</div>
+ 					</li>
+ 					<li>
+ 						<div class="commenterImage">
+ 							<img src="https://dummyimage.com/50x50/d3d3d3/fff" />
+ 						</div>
+ 						<div class="commentText">
+ 							<p class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio neque voluptas expedita obcaecati blanditiis distinctio numquam libero est placeat laborum repellendus consequatur, odit veniam corporis rerum iure commodi minus sunt.</p> <span class="date sub-text">Le 11/06/2018</span>
+
+ 						</div>
+ 					</li>
+ 					<li>
+ 						<div class="commenterImage">
+ 							<img src="https://dummyimage.com/50x50/d3d3d3/fff" />
+ 						</div>
+ 						<div class="commentText">
+ 							<p class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio neque voluptas expedita obcaecati blanditiis distinctio numquam libero est placeat laborum repellendus consequatur, odit veniam corporis rerum iure commodi minus sunt.</p> <span class="date sub-text">Le 11/06/2018</span>
+
+ 						</div>
+ 					</li>
+ 					<li>
+ 						<div class="commenterImage">
+ 							<img src="https://dummyimage.com/50x50/d3d3d3/fff" />
+ 						</div>
+ 						<div class="commentText">
+ 							<p class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio neque voluptas expedita obcaecati blanditiis distinctio numquam libero est placeat laborum repellendus consequatur, odit veniam corporis rerum iure commodi minus sunt.</p> <span class="date sub-text">Le 11/06/2018</span>
+
+ 						</div>
+ 					</li>
+ 					<li>
+ 						<div class="commenterImage">
+ 							<img src="https://dummyimage.com/50x50/d3d3d3/fff" />
+ 						</div>
+ 						<div class="commentText">
+ 							<p class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio neque voluptas expedita obcaecati blanditiis distinctio numquam libero est placeat laborum repellendus consequatur, odit veniam corporis rerum iure commodi minus sunt.</p> <span class="date sub-text">Le 11/06/2018</span>
+
+ 						</div>
+ 					</li>
+ 				</ul>
+ 				<form class="form-inline" role="form">
+ 					<div class="form-group">
+ 						<input class="form-control" type="text" placeholder="Votre commentaire" />
+ 					</div>
+ 					<div class="form-group">
+ 						<button class="btn btn-outline-default">Commenter</button>
+ 					</div>
+ 				</form>
+ 			</div>
+ 		</div>
+ 	</div>
+ 	<?php include('footer.php'); ?>
+
