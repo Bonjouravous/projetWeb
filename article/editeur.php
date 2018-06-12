062ce95b5a2b05806c7b79b336e3eebb8875ad16
Thanh Tung Nguyen <39303840+thenjip@users.noreply.github.com>
2018-06-12 09:09:15 +0200

    Basic editor for title and description

--- article/editeur.php
+++ article/editeur.php
@@ -5,17 +5,31 @@
 
 <?php
   /* A définir: "$lieu_id". */
-  $lieu_query = $bdd->query('SELECT Lieu.Nom, LieuDescription.Description FROM Lieu, LieuDescription WHERE Lieu.Id = LieuDescription.IdLieu AND Lieu.Id = '.$lieu_id.';');
+  $lieu_query = $bdd->query(
+    'SELECT Lieu.Nom, LieuDescription.Description'
+    .' FROM Lieu, LieuDescription'
+    .' WHERE Lieu.Id = LieuDescription.IdLieu AND Lieu.Id = '.$lieu_id
+    .';'
+  );
+  if ($lieu_query->rowCount() == 0) {
+    $lieu_titre = '';
+    $lieu_desc = '';
+  } else {
+    $lieu_titre = $lieu_query['nom'];
+    $lieu_desc = $lieu_query['description'];
+  }
 ?>
 
 <form id="article_form" action="traitement.php" method="post">
   <fieldset>
     <p>Titre:</p>
-    <input type="text" name="title" value=<?php echo '"'.$lieu_titre.'"' ?>/>
+    <input type="text" name="title" value=<?php echo '"'.$lieu_titre.'"'; ?>/>
   </fieldset>
     <p>Description:</p>
     <textarea name="description" form="article_form">
-      
+      <?php
+        echo $lieu_desc;
+      ?>
     </textarea>
   <fieldset>
   </fieldset>
