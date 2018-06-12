b1b900deb2697810759c386b697dbe8ec1334f7c
MiraLiantsoa <40161266+MiraLiantsoa@users.noreply.github.com>
2018-06-12 12:35:41 +0400

    Add files via upload

--- localisation.php
+++ localisation.php
@@ -1,40 +1,30 @@
-<?php
-include('header.php');
-?>
-<form id="sampleForm" name="sampleForm" method="get" action="recherche.php">
-<p>
-<label for="lat">Lattitude :</label>
-<input type="text" name="lat" id="lat" value="">
+<script type="text/javascript">
 
-<label for="long">Longitude :</label>
-<input type="text" name="long" id="long" value="">
-
-<label for="distance">Distance :</label>
-<input type="text" name="distance" value="2000">m
-</p>
-<p>
-<input type = 'button' onclick = 'geoloc()' value = 'Me localiser'>
-<input type="submit" value="Rechercher" />
-</p>
-</form>
-
-<script>
-function success(position) {
-	var lat = position.coords.latitude;
-	var long = position.coords.longitude;
-	document.sampleForm.lat.value = lat;
-	document.sampleForm.long.value = long;
-};
-
-function error(err) {
-	console.warn(`ERROR(${err.code}): ${err.message}`);
-};
+function erreur( error ) {
+	switch( error.code ) {
+		case error.PERMISSION_DENIED:
+			console.log( 'L\'utilisateur a refusé la demande' );
+			break;     
+		case error.POSITION_UNAVAILABLE:
+			console.log( 'Position indéterminée' );
+			break;
+		case error.TIMEOUT:
+			console.log( 'Réponse trop lente' );
+			break;
+	}
 	
-function geoloc() {
-	navigator.geolocation.getCurrentPosition(success, error);
 };
-</script>
 
-<?php
-include('footer.php');
-?>
+function callback( position ) {
+    var lat = position.coords.latitude;
+    var lng = position.coords.longitude;
+    console.log( lat, lng );
+	document.write(lat,';',lng);
+}
+
+
+if ( navigator.geolocation ) {
+	// On demande d'envoyer la position courante à la fonction callback
+	navigator.geolocation.getCurrentPosition( callback, erreur );
+} 
+</script>
