a3cf56881c3593681f9058d94658d56a046d1d35
Ambroise Renaud <ambroise.renaud@gmail.com>
2018-06-12 20:42:37 +0200

    edit profil

---
+++ user_edit.php
@@ -0,0 +1,19 @@
+<?php
+include('headermin.php');
+if(isset($_POST['envoi'])){
+		if (!empty($_POST['description'])){
+			$description = $_POST['description'];
+			$pseudo = $_SESSION['pseudo'];
+			$req = $bdd->prepare("UPDATE utilisateur SET description = :description WHERE pseudo = :pseudo");
+			$req->execute(array('description' => $description,'pseudo' => $pseudo));
+		}
+		if (!empty($_POST['photo'])){
+			$image = $_POST['photo'];
+			$pseudo = $_SESSION['pseudo'];
+			$req = $bdd->prepare("UPDATE utilisateur SET image = :image WHERE pseudo = :pseudo");
+			$req->execute(array('image' => $image,'pseudo' => $pseudo));
+		}
+		header("Location: http://localhost/user_editprofil.php?username=".$_SESSION['pseudo']);
+	}
+	
+?>
--- user_profil.php
+++ user_profil.php
@@ -1,19 +1,24 @@
- <?php include('header.php'); ?>
+ <?php include('header.php');
+
+$req = $bdd->prepare('SELECT pseudo, description, image FROM utilisateur WHERE pseudo = ?');
+$req->execute(array($_GET['username']));
+$donnees = $req->fetch();
+?>
  		<div class="card text-center">
  			<div class="card-header">
  				<ul class="nav nav-tabs card-header-tabs">
  					<li class="nav-item">
- 						<a class="nav-link active" href="user_profil.php">Mon profil</a>
+ 						<a class="nav-link active" href="user_profil.php">Profil</a>
  					</li>
  					<li class="nav-item">
- 						<a class="nav-link" href="user_editprofil.php">Editer mon profil</a>
+ 						<a class="nav-link" href="user_editprofil.php<?php echo "?username=".$_SESSION['pseudo'] ?>">Editer mon profil</a>
  					</li>
  				</ul>
  			</div>
  			<div class="card-body">
- 				<img src="https://dummyimage.com/200x200/d3d3d3/fff" class="img-thumbnail mb-3">
- 				<h5 class="card-title">Nom de l'utilisateur</h5>
- 				<p class="card-text">Description Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi ullam nemo, ut sequi reprehenderit iste debitis dolore fugit blanditiis saepe consequatur nulla excepturi. Ipsam eos, totam sed facere voluptate assumenda!</p>
+ 				<img height="200" width="200" src="<?php echo $donnees['image']; ?>" class="img-thumbnail mb-3">
+ 				<h5 class="card-title"><?php echo $donnees['pseudo']; ?></h5>
+ 				<p class="card-text"><?php echo $donnees['description']; ?></p>
 <!--  				<a href="#" class="btn btn-outline-success">Voir tout les postes</a>recherche avec comme attribut le nom de l'auteur --> 
  			</div>
  	</div>
