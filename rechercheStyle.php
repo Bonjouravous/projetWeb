37b60911dc5d16f2acf3bf2535de1a4fa7ef9a9d
ambroisernd <ambroise.renaud@gmail.com>
2018-06-12 11:46:33 +0200

    recherche fix

--- rechercheStyle.php
+++ rechercheStyle.php
@@ -25,6 +25,7 @@
 					<a href="#" class="card-link">Voir</a>
 					<a href="#" class="card-link">Modifier</a>
 					<a href="#" class="card-link">Supprimer</a>
+					<div class="text-right text-muted">#tag1 #tag2 #tag3</div>
 				</div>
 			</div>
 		</div>
@@ -38,127 +39,124 @@
 					<a href="#" class="card-link">Voir</a>
 					<a href="#" class="card-link">Modifier</a>
 					<a href="#" class="card-link">Supprimer</a>
+					<div class="text-right text-muted">#tag1 #tag2 #tag3</div>
 				</div>
 			</div>
-		</div> 	<div class="row">
-			<div class="col-lg-4" style="padding-top: 2%;padding-bottom: 2%">
-				<div class="card">
+		</div>
+		<div class="col-lg-4" style="padding-top: 2%;padding-bottom: 2%">
+			<div class="card">
 
-					<div class="card-body">
-						<h5 class="card-title">Nom du lieu</h5>
-						<h6 class="card-subtitle mb-2 text-muted">localisation</h6>
-						<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis ad ut vero. Iusto tempore eveniet laborum veniam quod, quas rerum temporibus quo et, sunt voluptates doloremque illo unde. Quia, aut.</p>
-						<a href="#" class="card-link">Voir</a>
-						<a href="#" class="card-link">Modifier</a>
-						<a href="#" class="card-link">Supprimer</a>
-					</div>
+				<div class="card-body">
+					<h5 class="card-title">Nom du lieu</h5>
+					<h6 class="card-subtitle mb-2 text-muted">localisation</h6>
+					<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis ad ut vero. Iusto tempore eveniet laborum veniam quod, quas rerum temporibus quo et, sunt voluptates doloremque illo unde. Quia, aut.</p>
+					<a href="#" class="card-link">Voir</a>
+					<a href="#" class="card-link">Modifier</a>
+					<a href="#" class="card-link">Supprimer</a>
+					<div class="text-right text-muted">#tag1 #tag2 #tag3</div>
 				</div>
 			</div>
-			<div class="col-lg-4" style="padding-top: 2%;padding-bottom: 2%">
-				<div class="card">
+		</div>
+		<div class="col-lg-4" style="padding-top: 2%;padding-bottom: 2%">
+			<div class="card">
 
-					<div class="card-body">
-						<h5 class="card-title">Nom du lieu</h5>
-						<h6 class="card-subtitle mb-2 text-muted">localisation</h6>
-						<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis ad ut vero. Iusto tempore eveniet laborum veniam quod, quas rerum temporibus quo et, sunt voluptates doloremque illo unde. Quia, aut.</p>
-						<a href="#" class="card-link">Voir</a>
-						<a href="#" class="card-link">Modifier</a>
-						<a href="#" class="card-link">Supprimer</a>
-					</div>
+				<div class="card-body">
+					<h5 class="card-title">Nom du lieu</h5>
+					<h6 class="card-subtitle mb-2 text-muted">localisation</h6>
+					<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis ad ut vero. Iusto tempore eveniet laborum veniam quod, quas rerum temporibus quo et, sunt voluptates doloremque illo unde. Quia, aut.</p>
+					<a href="#" class="card-link">Voir</a>
+					<a href="#" class="card-link">Modifier</a>
+					<a href="#" class="card-link">Supprimer</a>
+					<div class="text-right text-muted">#tag1 #tag2 #tag3</div>
 				</div>
 			</div>
-			<div class="col-lg-4" style="padding-top: 2%;padding-bottom: 2%">
-				<div class="card">
+		</div>
+		<div class="col-lg-4" style="padding-top: 2%;padding-bottom: 2%">
+			<div class="card">
 
-					<div class="card-body">
-						<h5 class="card-title">Nom du lieu</h5>
-						<h6 class="card-subtitle mb-2 text-muted">localisation</h6>
-						<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis ad ut vero. Iusto tempore eveniet laborum veniam quod, quas rerum temporibus quo et, sunt voluptates doloremque illo unde. Quia, aut.</p>
-						<a href="#" class="card-link">Voir</a>
-						<a href="#" class="card-link">Modifier</a>
-						<a href="#" class="card-link">Supprimer</a>
-					</div>
+				<div class="card-body">
+					<h5 class="card-title">Nom du lieu</h5>
+					<h6 class="card-subtitle mb-2 text-muted">localisation</h6>
+					<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis ad ut vero. Iusto tempore eveniet laborum veniam quod, quas rerum temporibus quo et, sunt voluptates doloremque illo unde. Quia, aut.</p>
+					<a href="#" class="card-link">Voir</a>
+					<a href="#" class="card-link">Modifier</a>
+					<a href="#" class="card-link">Supprimer</a>
+					<div class="text-right text-muted">#tag1 #tag2 #tag3</div>
 				</div>
 			</div>
-			<div class="row">
-				<div class="col-lg-4" style="padding-top: 2%;padding-bottom: 2%">
-					<div class="card">
+		</div>
+		<div class="col-lg-4" style="padding-top: 2%;padding-bottom: 2%">
+			<div class="card">
 
-						<div class="card-body">
-							<h5 class="card-title">Nom du lieu</h5>
-							<h6 class="card-subtitle mb-2 text-muted">localisation</h6>
-							<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis ad ut vero. Iusto tempore eveniet laborum veniam quod, quas rerum temporibus quo et, sunt voluptates doloremque illo unde. Quia, aut.</p>
-							<a href="#" class="card-link">Voir</a>
-							<a href="#" class="card-link">Modifier</a>
-							<a href="#" class="card-link">Supprimer</a>
-						</div>
-					</div>
+				<div class="card-body">
+					<h5 class="card-title">Nom du lieu</h5>
+					<h6 class="card-subtitle mb-2 text-muted">localisation</h6>
+					<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis ad ut vero. Iusto tempore eveniet laborum veniam quod, quas rerum temporibus quo et, sunt voluptates doloremque illo unde. Quia, aut.</p>
+					<a href="#" class="card-link">Voir</a>
+					<a href="#" class="card-link">Modifier</a>
+					<a href="#" class="card-link">Supprimer</a>
+					<div class="text-right text-muted">#tag1 #tag2 #tag3</div>
 				</div>
-				<div class="col-lg-4" style="padding-top: 2%;padding-bottom: 2%">
-					<div class="card">
+			</div>
+		</div>
+		<div class="col-lg-4" style="padding-top: 2%;padding-bottom: 2%">
+			<div class="card">
 
-						<div class="card-body">
-							<h5 class="card-title">Nom du lieu</h5>
-							<h6 class="card-subtitle mb-2 text-muted">localisation</h6>
-							<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis ad ut vero. Iusto tempore eveniet laborum veniam quod, quas rerum temporibus quo et, sunt voluptates doloremque illo unde. Quia, aut.</p>
-							<a href="#" class="card-link">Voir</a>
-							<a href="#" class="card-link">Modifier</a>
-							<a href="#" class="card-link">Supprimer</a>
-						</div>
-					</div>
+				<div class="card-body">
+					<h5 class="card-title">Nom du lieu</h5>
+					<h6 class="card-subtitle mb-2 text-muted">localisation</h6>
+					<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis ad ut vero. Iusto tempore eveniet laborum veniam quod, quas rerum temporibus quo et, sunt voluptates doloremque illo unde. Quia, aut.</p>
+					<a href="#" class="card-link">Voir</a>
+					<a href="#" class="card-link">Modifier</a>
+					<a href="#" class="card-link">Supprimer</a>
+					<div class="text-right text-muted">#tag1 #tag2 #tag3</div>
 				</div>
-				<div class="col-lg-4" style="padding-top: 2%;padding-bottom: 2%">
-					<div class="card">
+			</div>
+		</div>
+		<div class="col-lg-4" style="padding-top: 2%;padding-bottom: 2%">
+			<div class="card">
 
-						<div class="card-body">
-							<h5 class="card-title">Nom du lieu</h5>
-							<h6 class="card-subtitle mb-2 text-muted">localisation</h6>
-							<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis ad ut vero. Iusto tempore eveniet laborum veniam quod, quas rerum temporibus quo et, sunt voluptates doloremque illo unde. Quia, aut.</p>
-							<a href="#" class="card-link">Voir</a>
-							<a href="#" class="card-link">Modifier</a>
-							<a href="#" class="card-link">Supprimer</a>
-						</div>
-					</div>
+				<div class="card-body">
+					<h5 class="card-title">Nom du lieu</h5>
+					<h6 class="card-subtitle mb-2 text-muted">localisation</h6>
+					<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis ad ut vero. Iusto tempore eveniet laborum veniam quod, quas rerum temporibus quo et, sunt voluptates doloremque illo unde. Quia, aut.</p>
+					<a href="#" class="card-link">Voir</a>
+					<a href="#" class="card-link">Modifier</a>
+					<a href="#" class="card-link">Supprimer</a>
+					<div class="text-right text-muted">#tag1 #tag2 #tag3</div>
 				</div>
-				<div class="row">
-					<div class="col-lg-4" style="padding-top: 2%;padding-bottom: 2%">
-						<div class="card">
+			</div>
+		</div>
+		<div class="col-lg-4" style="padding-top: 2%;padding-bottom: 2%">
+			<div class="card">
 
-							<div class="card-body">
-								<h5 class="card-title">Nom du lieu</h5>
-								<h6 class="card-subtitle mb-2 text-muted">localisation</h6>
-								<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis ad ut vero. Iusto tempore eveniet laborum veniam quod, quas rerum temporibus quo et, sunt voluptates doloremque illo unde. Quia, aut.</p>
-								<a href="#" class="card-link">Voir</a>
-								<a href="#" class="card-link">Modifier</a>
-								<a href="#" class="card-link">Supprimer</a>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-4" style="padding-top: 2%;padding-bottom: 2%">
-						<div class="card">
-
-							<div class="card-body">
-								<h5 class="card-title">Nom du lieu</h5>
-								<h6 class="card-subtitle mb-2 text-muted">localisation</h6>
-								<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis ad ut vero. Iusto tempore eveniet laborum veniam quod, quas rerum temporibus quo et, sunt voluptates doloremque illo unde. Quia, aut.</p>
-								<a href="#" class="card-link">Voir</a>
-								<a href="#" class="card-link">Modifier</a>
-								<a href="#" class="card-link">Supprimer</a>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-4" style="padding-top: 2%;padding-bottom: 2%">
-						<div class="card">
-
-							<div class="card-body">
-								<h5 class="card-title">Nom du lieu</h5>
-								<h6 class="card-subtitle mb-2 text-muted">localisation</h6>
-								<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis ad ut vero. Iusto tempore eveniet laborum veniam quod, quas rerum temporibus quo et, sunt voluptates doloremque illo unde. Quia, aut.</p>
-								<a href="#" class="card-link">Voir</a>
-								<a href="#" class="card-link">Modifier</a>
-								<a href="#" class="card-link">Supprimer</a>
-							</div>
-						</div>
-					</div>
+				<div class="card-body">
+					<h5 class="card-title">Nom du lieu</h5>
+					<h6 class="card-subtitle mb-2 text-muted">localisation</h6>
+					<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis ad ut vero. Iusto tempore eveniet laborum veniam quod, quas rerum temporibus quo et, sunt voluptates doloremque illo unde. Quia, aut.</p>
+					<a href="#" class="card-link">Voir</a>
+					<a href="#" class="card-link">Modifier</a>
+					<a href="#" class="card-link">Supprimer</a>
+					<div class="text-right text-muted">#tag1 #tag2 #tag3</div>
 				</div>
-				<?php include('footer.php'); ?>
+			</div>
+		</div>
+		<div class="col-lg-4" style="padding-top: 2%;padding-bottom: 2%">
+			<div class="card">
+
+				<div class="card-body">
+					<h5 class="card-title">Nom du lieu</h5>
+					<h6 class="card-subtitle mb-2 text-muted">localisation</h6>
+					<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis ad ut vero. Iusto tempore eveniet laborum veniam quod, quas rerum temporibus quo et, sunt voluptates doloremque illo unde. Quia, aut.</p>
+					<a href="#" class="card-link">Voir</a>
+					<a href="#" class="card-link">Modifier</a>
+					<a href="#" class="card-link">Supprimer</a>
+					<div class="text-right text-muted">#tag1 #tag2 #tag3</div>
+				</div>
+			</div>
+		</div>
+		
+</div>
+
+	</div>
+	<?php include('footer.php'); ?>
