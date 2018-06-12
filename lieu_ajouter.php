fc1334949101ff7ceb8a47f1e5f7531f26a9c433
thenjip <39303840+thenjip@users.noreply.github.com>
2018-06-12 21:33:33 +0200

    Traitement fait sur place

--- lieu_ajouter.php
+++ lieu_ajouter.php
@@ -1,12 +1,50 @@
 <?php
 	include('header.php');
+	
+	$idlieu = isset($_GET['lieu']) ? $_GET['lieu'] : 'alpha';
+
+if(!is_numeric($idlieu)) {
+	echo 'Page non trouvée';
+} else {
+
 ?>
 
+<?php
+	$hassend = false;
+	$haserror = false;
 
-<form id="article_form" action="lieu_ajouter_traitement.php" method="post">
+	if (isset($_POST['add'])) {
+		$lieu_stmt = $bdd->prepare(
+			'INSERT INTO lieu(nom) VALUES (?);'
+		);
+		$lieu_desc_stmt = $bdd->prepare(
+			'INSERT_INTO lieudescription(description, idutilisateur, idlieu)'
+			.' VALUES (?, ?, ?);'
+		);
+		try {
+			$bdd->beginTransaction();
+			$lieu_stmt->execute(array($_POST['title']));
+			$last_idlieu = $bdd->lastInsertId();
+			$lieu_desc_stmt = $bdd->execute(
+				array($_POST['description'], $_SESSION['id'], $last_idlieu
+			);
+			$bdd->commit();
+			$hassend = true;
+		} catch (PDOException $e) {
+			$bdd->rollback();
+			echo '<p>'.$e->getMessage().'</p>';
+		}
+	}
+
+	if ($hassend && !$haserror) {
+		
+	} else {
+?>
+
+<form id="article_form" action="lieu_ajouter.php" method="post">
 	<fieldset>
 		<p>Titre:</p>
-		<input type="text" name="title" value=""/>
+		<input type="text" name="title" value="" />
 	</fieldset>
 	<fieldset>
 		<div>
@@ -24,7 +62,7 @@
 	</div>
 	</fieldset>
 	<fieldset>
-		<input type="submit" name="add" value="Créer"/>
+		<input type="submit" name="add" value="Ajouter"/>
 	</fieldset>
 </form>
 
@@ -33,6 +71,7 @@
 
 <?php
 	}
+}
 
 	include('footer.php');
 ?>
--- lieu_edit.php
+++ lieu_edit.php
@@ -9,21 +9,47 @@ if(!is_numeric($idlieu)) {
 
 ?>
 
-
 <?php
-	$lieu_query = $bdd->query(
-		'SELECT lieu.nom, lieudescription.description'
-		.' FROM lieu, lieudescription'
-		.' WHERE lieu.id = lieudescription.idlieu AND lieu.id = '.$lieu_id
-		.';'
-	);
-	$lieu_query->execute();
-	$data = $lieu_query->fetch();
-	$lieu_titre = $data['nom'];
-	$lieu_desc = $data['description'];
+	$hassend = false;
+	$haserror = false;
+
+	if (isset($_POST['update'])) {
+		$lieu_stmt = $bdd->prepare(
+			'UPDATE lieu SET nom = ? WHERE id = ?;'
+		);
+		$lieu_desc_stmt = $bdd->prepare(
+			'UPDATE lieudescription SET description = ?, idutilisateur = ? WHERE idlieu = ?;'
+		);
+		try {
+			$bdd->beginTransaction();
+			$lieu_stmt->execute(array($_POST['title'], $_GET['idlieu']));
+			$lieu_desc_stmt = $bdd->execute(
+				array($_POST['description'], $_SESSION['id'], $_GET['idlieu']
+			);
+			$bdd->commit();
+			$hassend = true;
+		} catch (PDOException $e) {
+			$bdd->rollback();
+			echo '<p>'.$e->getMessage().'</p>';
+		}
+	}
+
+	if ($hassend && !$haserror) {
+		
+	} else {
+		$lieu_query = $bdd->query(
+			'SELECT lieu.nom, lieudescription.description'
+			.' FROM lieu, lieudescription'
+			.' WHERE lieu.id = lieudescription.idlieu AND lieu.id = '.$lieu_id
+			.';'
+		);
+		$lieu_query->execute();
+		$data = $lieu_query->fetch();
+		$lieu_titre = $data['nom'];
+		$lieu_desc = $data['description'];
 ?>
 
-<form id="article_form" action="lieu_edit_traitement.php" method="post">
+<form id="article_form" action="lieu_edit.php" method="post">
 	<fieldset>
 		<p>Titre:</p>
 		<input type="text" name="title" value=<?php echo '"'.$lieu_titre.'"'; ?> />
@@ -55,6 +81,7 @@ if(!is_numeric($idlieu)) {
 
 
 <?php
+	}
 }
 
 	include('footer.php');
