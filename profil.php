74a01c50e90042ed805a7dcd4a456600f8b2ae2f
ambroisernd <ambroise.renaud@gmail.com>
2018-06-12 11:00:43 +0200

    profil

---
+++ profil.php
@@ -0,0 +1,24 @@
+ <?php include('header.php'); ?>
+ <div class="container">
+ 		<div class="card text-center">
+ 			<div class="card-header">
+ 				<ul class="nav nav-tabs card-header-tabs">
+ 					<li class="nav-item">
+ 						<a class="nav-link active" href="profil.php">Mon profil</a>
+ 					</li>
+ 					<li class="nav-item">
+ 						<a class="nav-link" href="editProfil.php">Editer mon profil</a>
+ 					</li>
+ 				</ul>
+ 			</div>
+ 			<div class="card-body">
+ 				<img src="https://dummyimage.com/200x200/d3d3d3/fff" class="img-thumbnail mb-3">
+ 				<h5 class="card-title">Nom de l'utilisateur</h5>
+ 				<p class="card-text">Description Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi ullam nemo, ut sequi reprehenderit iste debitis dolore fugit blanditiis saepe consequatur nulla excepturi. Ipsam eos, totam sed facere voluptate assumenda!</p>
+<!--  				<a href="#" class="btn btn-outline-success">Voir tout les postes</a>recherche avec comme attribut le nom de l'auteur --> 
+ 			</div>
+ 	</div>
+ </div>
+<div class="fixed-bottom">
+ <?php include('footer.php'); ?>
+ </div>
