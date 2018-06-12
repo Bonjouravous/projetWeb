b0378e52a231906fc22c822637d44803967bda54
Thanh Tung Nguyen <thanh-tung.nguyen@uha.fr>
2018-06-12 11:01:25 +0200

    Start fixing regex exprs.

--- description.php
+++ description.php
@@ -15,13 +15,13 @@
 
   $lieu_desc = $lieu_desc_query['description'];
   $lieu_desc = preg_replace(
-    '\\*\\* \\(.*?\\) \\*\\*', '<h2>$1<h2>', $lieu_desc
+    '/[*][*] \\(.*?\\) [*][*]/', '<h2>$1<h2>', $lieu_desc
   );
   $lieu_desc = preg_replace(
-    '\\*\\*\\* \\(.*?\\) \\*\\*\\*' , '<h3>$1<h3>', $lieu_desc
+    '/[*][*][*] \\(.*?\\) [*][*][*]/' , '<h3>$1<h3>', $lieu_desc
   );
   $lieu_desc = preg_replace(
-    '\\[\\[(.*?)|(.*?)\\]\\]', '<a href="$1">$2</a>', $lieu_desc
+    '/\\[\\[(.*?)[|](.*?)\\]\\]/', '<a href="$1">$2</a>', $lieu_desc
   );
 
   $lieu_desc_date = $lieu_desc_query['date'];
