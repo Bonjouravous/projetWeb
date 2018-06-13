c05ba8164fe88aa96604ee01d9e25eb45c904237
Ambroise Renaud <ambroise.renaud@gmail.com>
2018-06-13 15:48:18 +0200

    deux fois email ou username fix

--- user_signup.php
+++ user_signup.php
@@ -104,51 +104,71 @@ include('headermin.php');
 </head>
 <body>
 	<div class="signup-form">
-<?php
-if(isset($_POST['pseudo']) && isset($_POST['email']) && isset($_POST['pass'])) {
-	$pass_hache = password_hash($_POST['pass'], PASSWORD_DEFAULT);
+		<?php
+		if(isset($_POST['pseudo']) && isset($_POST['email']) && isset($_POST['pass'])) {
+			$requete = $bdd->prepare('SELECT pseudo from utilisateur WHERE pseudo = :pseudo');
+			$requete->execute(array('pseudo' => $_POST['pseudo']));
+			$rep = $requete->fetch();
+			$requete2 = $bdd->prepare('SELECT pseudo from utilisateur WHERE mail = :email');
+			$requete2->execute(array('email' => $_POST['email']));
+			$rep2 = $requete2->fetch();
+			if($rep){
+				echo '		<form>
+				Cet utilisateur existe déjà !
+				</form>
+				<div class="text-center" style="color:#999;"><a href="user_signup.php" style="color:#999;">Réessayer</a></div>';
+			}
+			if($rep2){
+				echo '		<form>
+				Cet e-mail est déjà utilisé !
+				</form>
+				<div class="text-center" style="color:#999;"><a href="user_signup.php" style="color:#999;">Réessayer</a></div>';
+			}
+			else{
+				$pass_hache = password_hash($_POST['pass'], PASSWORD_DEFAULT);
 
 	// Insertion
-	$req = $bdd->prepare('INSERT INTO `utilisateur` (`id`, `pseudo`, `mdp`, `image`, `description`, `mail`, `inscription`, `moderateur`, `banni`) VALUES (NULL, :pseudo, :mdp, NULL, \'\', :mail, CURDATE(), 0, 0)');
-	$req->execute(array(
-		'pseudo' => $_POST['pseudo'],
-		'mdp' => $pass_hache,
-		'mail' =>$_POST['email']));
-	?>
-	<form>
-	Votre compte a bien été créé!
-	<div class="form-group">
-		<a href="user_login.php" class="btn btn-success btn-lg btn-block">Se connecter</a>
-	</div>
-	</form>
-	<?php
-} else {
-?>
-		<form action="user_signup.php" method="post">
-			<h2>S'inscrire</h2>
-			<p class="hint-text">Créer un compte pour partager de nouveaux lieux !</p>
-			<div class="form-group">
-				<input type="text" class="form-control" name="pseudo" placeholder="Nom d'utilisateur" required="required">       	
-			</div>
-			<div class="form-group">
-				<input type="email" class="form-control" name="email" placeholder="Email" required="required">
-			</div>
-			<div class="form-group">
-				<input type="password" class="form-control" name="pass" placeholder="Mot de passe" required="required">
-			</div>       
-			<div class="form-group">
-				<button type="submit" class="btn btn-success btn-lg btn-block">S'inscrire</button>
-			</div>
-		</form>
-		<div class="text-center" style="color: #999;">Vous avez deja un compte? <a href="user_login.php" style="color:#999;">Se connecter</a></div>
-		
-<?php } ?>
-	</div>
+				$req = $bdd->prepare('INSERT INTO `utilisateur` (`id`, `pseudo`, `mdp`, `image`, `description`, `mail`, `inscription`, `moderateur`, `banni`) VALUES (NULL, :pseudo, :mdp, NULL, \'\', :mail, CURDATE(), 0, 0)');
+				$req->execute(array(
+					'pseudo' => $_POST['pseudo'],
+					'mdp' => $pass_hache,
+					'mail' =>$_POST['email']));
+				echo '<form>
+				Votre compte a bien été créé!
+				<div class="form-group">
+				<a href="user_login.php" class="btn btn-success btn-lg btn-block">Se connecter</a>
+				</div>
+				</form>';
 
-	<!-- Optional JavaScript -->
-	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-	<script src="js/jquery-3.2.1.slim.min.js"></script>
-	<script src="js/popper.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-</body>
-</html>
+			}
+		} 
+		else {
+			?>
+			<form action="user_signup.php" method="post">
+				<h2>S'inscrire</h2>
+				<p class="hint-text">Créer un compte pour partager de nouveaux lieux !</p>
+				<div class="form-group">
+					<input type="text" class="form-control" name="pseudo" placeholder="Nom d'utilisateur" required="required">       	
+				</div>
+				<div class="form-group">
+					<input type="email" class="form-control" name="email" placeholder="Email" required="required">
+				</div>
+				<div class="form-group">
+					<input type="password" class="form-control" name="pass" placeholder="Mot de passe" required="required">
+				</div>       
+				<div class="form-group">
+					<button type="submit" class="btn btn-success btn-lg btn-block">S'inscrire</button>
+				</div>
+			</form>
+			<div class="text-center" style="color: #999;">Vous avez deja un compte? <a href="user_login.php" style="color:#999;">Se connecter</a></div>
+
+			<?php } ?>
+		</div>
+
+		<!-- Optional JavaScript -->
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="js/jquery-3.2.1.slim.min.js"></script>
+		<script src="js/popper.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+	</body>
+	</html>
