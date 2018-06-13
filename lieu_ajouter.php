dc8ef45f3695277924e378e84ab28f5a2798723a
ambroisernd <ambroise.renaud@gmail.com>
2018-06-13 18:18:07 +0200

    ouais ouais

--- lieu_ajouter.php
+++ lieu_ajouter.php
@@ -1,88 +1,104 @@
 <?php
-	include('header.php');
+include('header.php');
 ?>
 
 <?php
-	$hassend = false;
-	$haserror = false;
+$hassend = false;
+$haserror = false;
 
-	if (isset($_POST['add'])) {
-		$haserror = empty($_POST['title']);
-		if (!$haserror) {
-			$lieu_stmt = $bdd->prepare(
-				'INSERT INTO `lieu` (`id`, `nom`, `latitude`, `longitude`, `creation`) VALUES (NULL, ?, ?, ?, CURRENT_DATE())'
+if (isset($_POST['add'])) {
+	$haserror = empty($_POST['title']);
+	if (!$haserror) {
+		$lieu_stmt = $bdd->prepare(
+			'INSERT INTO `lieu` (`id`, `nom`, `latitude`, `longitude`, `creation`) VALUES (NULL, ?, ?, ?, CURRENT_DATE())'
+		);
+		$lieu_desc_stmt = $bdd->prepare(
+			'INSERT INTO lieudescription(date, description, idlieu, idutilisateur)'
+			.' VALUES (NOW(), ?, ?, ?)'
+			.';'
+		);
+		try {
+			$bdd->beginTransaction();
+			$lieu_stmt->execute(array($_POST['title'], $_POST['latitude'], $_POST['longitude']));
+			$last_idlieu = $bdd->lastInsertId();
+			$lieu_desc_stmt->execute(
+				array($_POST['description'], $last_idlieu, $_SESSION['id'])
 			);
-			$lieu_desc_stmt = $bdd->prepare(
-				'INSERT INTO lieudescription(date, description, idlieu, idutilisateur)'
-				.' VALUES (NOW(), ?, ?, ?)'
-				.';'
-			);
-			try {
-				$bdd->beginTransaction();
-				$lieu_stmt->execute(array($_POST['title'], $_POST['latitude'], $_POST['longitude']));
-				$last_idlieu = $bdd->lastInsertId();
-				$lieu_desc_stmt->execute(
-					array($_POST['description'], $last_idlieu, $_SESSION['id'])
-				);
-				$bdd->commit();
-				$hassend = true;
-			} catch (PDOException $e) {
-				$bdd->rollback();
-				echo '<p>'.$e->getMessage().'</p>';
-			}
+			$bdd->commit();
+			$hassend = true;
+		} catch (PDOException $e) {
+			$bdd->rollback();
+			echo '<p>'.$e->getMessage().'</p>';
 		}
-		$hassend = true;
 	}
+	$hassend = true;
+}
 
-	if ($hassend && !$haserror) {
+if ($hassend && !$haserror) {
 	?>
 	<p>Votre lieu a bien été ajouté.</p>
 	<p><a href="lieu_voir.php?lieu=<?php echo $last_idlieu; ?>">Accéder au lieu</a></p>
 	<?php
-	} else {
-?>
+} else {
+	?>
 
-<div class="card p-4" >
-	<form class="text-center" id="lieu_form" action="lieu_ajouter.php" method="post">
-		<div class="row">
-			<div class="col-md-12">
-				<div class="form-group">
-					<label for="inputemail">Titre</label>
-					<input type="text" class="form-control" id="title" name="title"/>
-				</div><!--/*.form-group-->
-			</div><!--/*.col-md-6-->
-			<div>
-				Latitude: <input type="number" name="latitude" step="0.01" value="0" min="0." max="90"/>
-				Longitute: <input type="number" name="longitude" step="0.01" value="0" min="-180" max="180"/>
+	<div class="card p-4" >
+		<form class="text-center" id="lieu_form" action="lieu_ajouter.php" method="post">
+			<div class="row">
+				<div class="col-xl-12">
+					<div class="form-group">
+						<label for="inputemail">Titre</label>
+						<input type="text" class="form-control" id="title" name="title"/>
+					</div><!--/*.form-group-->
+				</div><!--/*.col-md-6-->
 			</div>
-			<div class="col-md-12">
-				<div class="form-group">
-					<label for="description">Description</label>
-					<div>
-						<p>La description supporte le formatage suivant:</p>
-						<ul>
-							<li>Titre principal: ** titre principal **</li>
-							<li>Titre secondaire: *** titre secondaire ***</li>
-							<li>Lien URL: [[http://...|texte à afficher]]</li>
-						</ul>
+			<div class="form-group">
+				<div class="row">
+					<div class="col-sm-6">
+						<label for="latitude">Latitude</label>
+						<input type="number" name="latitude" step="0.01" value="0" min="0." max="90" class="form-control"/>
 					</div>
-					<div>
-						<p>Description:</p>
-						<textarea id="description" class="form-control" name="description" form="lieu_form"></textarea>
+					<div class="col-sm-6">
+						<label for="longitude">Longitude</label><input type="number" name="longitude" step="0.01" value="0" min="-180" max="180" class="form-control"/>
 					</div>
-				</div><!--/*.form-group-->
-			</div><!--/*.col-md-12-->
-			<div class="col-md-12">
-				<button type="submit" class='btn btn-success' name="add">Publier</button>
-				<a role="button" onClick="history.go(-1);" class='btn btn-danger'>Annuler</a>
-			</div><!--/*.col-md-12-->
-		</div><!--/*.row-->
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xl-12">
+					<div class="form-group">
+
+						<div class="card border-secondary mb-3">
+							<div class="card-header">Description</div>
+							<div class="card-body text-secondary">
+								<h5 class="card-title">La description supporte le formatage suivant</h5>
+								<p class="card-text" >						<ul style="list-style-type: none;">
+									<li>** titre principal **</li>
+									<li>*** titre secondaire ***</li>
+									<li>[[<small>http://...</small>|texte à afficher]]</li>
+								</ul></p>
+							</div>
+						</div>
+						<div>
+							<label for="description">Description</label>
+							<textarea id="description" class="form-control" name="description" form="lieu_form"></textarea>
+						</div>
+					</div><!--/*.form-group-->
+				</div><!--/*.col-md-12-->
+			</div>
+			<div class="row">
+				<div class="col-md-12">
+					<button type="submit" class='btn btn-success' name="add">Publier</button>
+					<a role="button" onClick="history.go(-1);" class='btn btn-danger' style="color: white;">Annuler</a>
+				</div><!--/*.col-md-12-->
+			</div><!--/*.row-->
+		</div>
 	</form>
 </div>
 
 
-<?php
-	}
 
-	include('footer.php');
+<?php
+}
+
+include('footer.php');
 ?>
--- lieu_edit.php
+++ lieu_edit.php
@@ -1,14 +1,14 @@
 <?php
-	include('header.php');
+include('header.php');
 
-	$idlieu = isset($_GET['lieu']) ? (int) $_GET['lieu'] : 'alpha';
+$idlieu = isset($_GET['lieu']) ? (int) $_GET['lieu'] : 'alpha';
 
 if(!is_numeric($idlieu)) {
 	echo 'Page non trouvée';
 } else {
-?>
+	?>
 
-<?php
+	<?php
 	$hassend = false;
 	$haserror = false;
 
@@ -42,10 +42,10 @@ if(!is_numeric($idlieu)) {
 	}
 
 	if ($hassend && !$haserror) {
-	?>
-	<p>Votre lieu a bien été édité.</p>
-	<p><a href="lieu_voir.php?lieu=<?php echo $idlieu; ?>">Accéder au lieu</a></p>
-	<?php
+		?>
+		<p>Votre lieu a bien été édité.</p>
+		<p><a href="lieu_voir.php?lieu=<?php echo $idlieu; ?>">Accéder au lieu</a></p>
+		<?php
 	} else {
 		$lieu_stmt = $bdd->prepare(
 			'SELECT lieu.nom AS titre, lieudescription.description AS description'
@@ -67,45 +67,48 @@ if(!is_numeric($idlieu)) {
 		$data = $lieu_stmt->fetch();
 		$lieu_titre = $data['titre'];
 		$lieu_desc = $data['description'];
-?>
+		?>
 
-<div class="card p-4" >
-	<form class="text-center" id="lieu_form" action="lieu_edit.php?lieu=<?=$idlieu?>" method="post" enctype="multipart/form-data">
-		<div class="row">
-			<div class="col-md-12">
-				<div class="form-group">
-					<label for="inputemail">Titre</label>
-					<input type="text" class="form-control" id="title" name="title" value=<?php echo '"'.$lieu_titre.'"'; ?> />
-				</div><!--/*.form-group-->
-			</div><!--/*.col-md-6-->
-			<div class="col-md-12">
-				<div class="form-group">
-					<label for="description">Description</label>
-					<div>
-						<p>La description supporte le formatage suivant:</p>
-						<ul>
-							<li>Titre principal: ** titre principal **</li>
-							<li>Titre secondaire: *** titre secondaire ***</li>
-							<li>Lien URL: [[http://...|texte à afficher]]</li>
-						</ul>
-					</div>
-					<div>
-						<p>Description:</p>
-						<textarea id="description" class="form-control" name="description" form="lieu_form"><?php echo $lieu_desc; ?></textarea>
-					</div>
-				</div><!--/*.form-group-->
-			</div><!--/*.col-md-12-->
-			<div class="col-md-12">
-				<button type="submit" class='btn btn-success' name="update">Mettre à jour</button>
-				<a role="button" href="lieu_voir.php?lieu=<?php echo $_GET['lieu']; ?>" class='btn btn-danger'>Annuler</a>
-			</div><!--/*.col-md-12-->
-		</div><!--/*.row-->
-	</form>
-</div>
+		<div class="card p-4" >
+			<form class="text-center" id="lieu_form" action="lieu_edit.php?lieu=<?=$idlieu?>" method="post" enctype="multipart/form-data">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="form-group">
+							<label for="inputemail">Titre</label>
+							<input type="text" class="form-control" id="title" name="title" value=<?php echo '"'.$lieu_titre.'"'; ?> />
+						</div><!--/*.form-group-->
+					</div><!--/*.col-md-6-->
+					<div class="col-md-12">
+						<div class="form-group">
+							<div>
+								<div class="card border-secondary mb-3">
+									<div class="card-header">Description</div>
+									<div class="card-body text-secondary">
+										<h5 class="card-title">La description supporte le formatage suivant</h5>
+										<p class="card-text" >						<ul style="list-style-type: none;">
+											<li>** titre principal **</li>
+											<li>*** titre secondaire ***</li>
+											<li>[[<small>http://...</small>|texte à afficher]]</li>
+										</ul></p>
+									</div>
+								</div>
+								<label for="description">Description</label>
+								<textarea id="description" class="form-control" name="description" form="lieu_form"><?php echo $lieu_desc; ?></textarea>
+							</div>
+						</div><!--/*.form-group-->
+					</div><!--/*.col-md-12-->
+					<div class="col-md-12">
+						<button type="submit" class='btn btn-success' name="update">Mettre à jour</button>
+						<a role="button" href="lieu_voir.php?lieu=<?php echo $_GET['lieu']; ?>" class='btn btn-danger'>Annuler</a>
+					</div><!--/*.col-md-12-->
+				</div><!--/*.row-->
+			</form>
+		</div>
 
-<?php
+		<?php
 	}
 }
 
-	include('footer.php');
+include('footer.php');
 ?>
+
