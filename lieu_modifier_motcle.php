7098dd4687b800398d9aaa701fee7cec91b0ad28
Thanh Tung Nguyen <thanh-tung.nguyen@uha.fr>
2018-06-14 16:45:38 +0200

    read and write to DB in one transaction

--- lieu_modifier_motcle.php
+++ lieu_modifier_motcle.php
@@ -11,76 +11,81 @@ if(!is_numeric($idlieu)) {
 	$hassend = false;
 	$haserror = false;
 
-	if (isset($_POST['save'])) {
-		try {
-			$bdd->beginTransaction();
-			$clear_stmt = $bdd->prepare('DELETE FROM lieumotcle WHERE idlieu = ?;');
-			$clear_stmt->execute(array($idlieu));
-			if (isset($_POST['tags'])) {
-				foreach ($_POST['tags'] as $tag) {
-					${$tag.'_stmt'} = $bdd->prepare(
-						'INSERT INTO lieumotcle(idlieu, idmot) VALUES (?, ?);'
-					);
-					${$tag.'_stmt'}->execute(array($idlieu, $tag));
+	try {
+		$bdd->beginTransaction();
+		if (isset($_POST['save'])) {
+			try {
+				$clear_stmt = $bdd->prepare('DELETE FROM lieumotcle WHERE idlieu = ?;');
+				$clear_stmt->execute(array($idlieu));
+				if (isset($_POST['tags'])) {
+					foreach ($_POST['tags'] as $tag) {
+						${$tag.'_stmt'} = $bdd->prepare(
+							'INSERT INTO lieumotcle(idlieu, idmot) VALUES (?, ?);'
+						);
+						${$tag.'_stmt'}->execute(array($idlieu, $tag));
+					}
 				}
+				$bdd->commit();
+				$hassend = true;
+			} catch (PDOException $e) {
+				$bdd->rollback();
+				echo '<p>'.$e->getMessage().'</p>';
 			}
-			$bdd->commit();
-			$hassend = true;
-		} catch (PDOException $e) {
-			$bdd->rollback();
-			echo '<p>'.$e->getMessage().'</p>';
 		}
-	}
 
-	if ($hassend && !$haserror) {
-		?>
-		<p>Mots-clés modifiés.</p>
-		<p><a href="lieu_voir.php?lieu=<?php echo $idlieu; ?>">Accéder au lieu</a></p>
-		<?php
-	} else {
-		?>
+		if ($hassend && !$haserror) {
+			?>
+			<p>Mots-clés modifiés.</p>
+			<p><a href="lieu_voir.php?lieu=<?php echo $idlieu; ?>">Accéder au lieu</a></p>
+			<?php
+		} else {
+			?>
 
-		<div class="card p-4" >
-			<div class="row">
-				<div class="col-md-12">
-					<form id="sampleForm" name="sampleForm" method="post" action="lieu_modifier_motcle.php?lieu=<?=$idlieu?>" class="form-group">
-						<select class="chosen_select form-control" name="tags[]" multiple>
-							<?php
-							$lieu_motcles_choice_stmt = $bdd->prepare(
-								'SELECT motcle.mot, motcle.id AS idmot, lieumotcle.id AS isselected'
-								.' FROM motcle LEFT JOIN lieumotcle ON lieumotcle.idmot = motcle.id AND lieumotcle.idlieu = ?'
-								.' ORDER BY motcle.mot ASC;'
-							);
-							try {
-								$bdd->beginTransaction();
-								$lieu_motcles_choice_stmt->execute(array($idlieu));
-								$bdd->commit();
-							} catch (PDOException $e) {
-								$bdd->rollback();
-								echo '<p>'.$e->getMessage().'</p>';
-							}
-							$lieu_motcles_choice_fetch = $lieu_motcles_choice_stmt->fetchAll(PDO::FETCH_ASSOC);
-
-							foreach($lieu_motcles_choice_fetch as $row) {
-								?>
-								<option <?php if (!is_null($row['isselected'])) { echo 'selected'; } ?> value="<?php echo $row['idmot']; ?>"><?php echo $row['mot']; ?></option>
+			<div class="card p-4" >
+				<div class="row">
+					<div class="col-md-12">
+						<form id="sampleForm" name="sampleForm" method="post" action="lieu_modifier_motcle.php?lieu=<?=$idlieu?>" class="form-group">
+							<select class="chosen_select form-control" name="tags[]" multiple>
 								<?php
-							}
-							?>
-						</select>
-						<button type="submit" class='btn btn-success mt-1' name="save">Enregistrer</button>
-						<a role="button" onClick="history.go(-1);" class='btn btn-danger mt-1' style="color: white;">Annuler</a>
-					</form>
-				</div><!--/*.col-md-12-->
-			</div><!--/*.row-->
-		</div>
+								$lieu_motcles_choice_stmt = $bdd->prepare(
+									'SELECT motcle.mot, motcle.id AS idmot, lieumotcle.id AS isselected'
+									.' FROM motcle LEFT JOIN lieumotcle ON lieumotcle.idmot = motcle.id AND lieumotcle.idlieu = ?'
+									.' ORDER BY motcle.mot ASC;'
+								);
+								try {
+									$bdd->beginTransaction();
+									$lieu_motcles_choice_stmt->execute(array($idlieu));
+									$bdd->commit();
+								} catch (PDOException $e) {
+									$bdd->rollback();
+									echo '<p>'.$e->getMessage().'</p>';
+								}
+								$lieu_motcles_choice_fetch = $lieu_motcles_choice_stmt->fetchAll(PDO::FETCH_ASSOC);
 
-
-		<?php
+								foreach($lieu_motcles_choice_fetch as $row) {
+									?>
+									<option <?php if (!is_null($row['isselected'])) { echo 'selected'; } ?> value="<?php echo $row['idmot']; ?>"><?php echo $row['mot']; ?></option>
+									<?php
+								}
+								?>
+							</select>
+							<button type="submit" class='btn btn-success mt-1' name="save">Enregistrer</button>
+							<a role="button" onClick="history.go(-1);" class='btn btn-danger mt-1' style="color: white;">Annuler</a>
+						</form>
+					</div><!--/*.col-md-12-->
+				</div><!--/*.row-->
+			</div>
+			<?php
+		}
+	} catch (PDOException $e) {
+		$bdd->rollback();
+		echo '<p>'.$e->getMessage().'</p>';
 	}
 }
+
 include('footer.php');
 ?>
+
 <script>
 $('.chosen_select').chosen({
 	width: "95%"
