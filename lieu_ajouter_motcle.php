39c54c88890e0de75b9fdb880e6c31e3afb58185
Thanh Tung Nguyen <thanh-tung.nguyen@uha.fr>
2018-06-14 10:26:21 +0200

    display keywords that are not already in the article

--- lieu_ajouter_motcle.php
+++ lieu_ajouter_motcle.php
@@ -3,6 +3,7 @@
 ?>
 
 <?php
+	$idlieu = (int) $_GET['lieu'];
 	$hassend = false;
 	$haserror = false;
 ?>
@@ -10,15 +11,28 @@
 <form id="sampleForm" name="sampleForm" method="post" action="lieu_ajouter_motcle.php" class="form-group">
 	<select class="chosen_select" name="tags[]" multiple>
 	<?php
-		$lieu_motcles_stmt = $bdd->prepare('SELECT mot FROM motcle WHERE ');
+		$lieu_motcles_choice_stmt = $bdd->prepare(
+			'SELECT motcle.mot as mot, motcle.id as id FROM motcle, lieumotcle'
+			.' WHERE lieumotcle.idlieu = ?'
+			.'  AND motcle.id NOT IN ('
+			.'    SELECT idmot FROM lieumotcle WHERE lieumotcle.idlieu = ?'
+			.'  )'
+			.';'
+		);
+		try {
+			$bdd->beginTransaction();
+			$lieu_motcles_choice_stmt->execute($idlieu, $idlieu);
+			$bdd->commit();
+		} catch (PDOException $e) {
+			$bdd->rollback();
+			echo '<p>'.$e->getMessage().'</p>';
+		}
+		$lieu_motcles_choice_fetch = $lieu_motcles_choice_stmt->fetchAll(PDO::FETCH_ASSOC);
 
-		$rep = $bdd->query('SELECT * FROM motcle;');
-		$rep->execute();
-		$motscles = $rep->fetchAll(PDO::FETCH_ASSOC);
-		print_r($motscles);
-		
-		foreach($motscles as $motcle) {
-			echo ("<option value=".$motcle['id'].">".$motcle['mot']."</option>");
+		foreach($lieu_motcles_choice_fetch as $motcle) {
+		?>
+			<option value="<?php echo $motcle['id']; ?>"><?php echo $motcle['mot']; ?></option>
+		<?php
 		}
 	?>
 	</select>
