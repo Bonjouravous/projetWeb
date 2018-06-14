558695e5e76b20cf2454aacd9729cfde2ec0ea14
Thanh Tung Nguyen <thanh-tung.nguyen@uha.fr>
2018-06-14 16:56:14 +0200

    fix reading and modifying keywords in one transaction

--- lieu_modifier_motcle.php
+++ lieu_modifier_motcle.php
@@ -14,23 +14,17 @@ if(!is_numeric($idlieu)) {
 	try {
 		$bdd->beginTransaction();
 		if (isset($_POST['save'])) {
-			try {
-				$clear_stmt = $bdd->prepare('DELETE FROM lieumotcle WHERE idlieu = ?;');
-				$clear_stmt->execute(array($idlieu));
-				if (isset($_POST['tags'])) {
-					foreach ($_POST['tags'] as $tag) {
-						${$tag.'_stmt'} = $bdd->prepare(
-							'INSERT INTO lieumotcle(idlieu, idmot) VALUES (?, ?);'
-						);
-						${$tag.'_stmt'}->execute(array($idlieu, $tag));
-					}
+			$clear_stmt = $bdd->prepare('DELETE FROM lieumotcle WHERE idlieu = ?;');
+			$clear_stmt->execute(array($idlieu));
+			if (isset($_POST['tags'])) {
+				foreach ($_POST['tags'] as $tag) {
+					${$tag.'_stmt'} = $bdd->prepare(
+						'INSERT INTO lieumotcle(idlieu, idmot) VALUES (?, ?);'
+					);
+					${$tag.'_stmt'}->execute(array($idlieu, $tag));
 				}
-				$bdd->commit();
-				$hassend = true;
-			} catch (PDOException $e) {
-				$bdd->rollback();
-				echo '<p>'.$e->getMessage().'</p>';
 			}
+			$hassend = true;
 		}
 
 		if ($hassend && !$haserror) {
@@ -46,28 +40,20 @@ if(!is_numeric($idlieu)) {
 					<div class="col-md-12">
 						<form id="sampleForm" name="sampleForm" method="post" action="lieu_modifier_motcle.php?lieu=<?=$idlieu?>" class="form-group">
 							<select class="chosen_select form-control" name="tags[]" multiple>
-								<?php
-								$lieu_motcles_choice_stmt = $bdd->prepare(
-									'SELECT motcle.mot, motcle.id AS idmot, lieumotcle.id AS isselected'
-									.' FROM motcle LEFT JOIN lieumotcle ON lieumotcle.idmot = motcle.id AND lieumotcle.idlieu = ?'
-									.' ORDER BY motcle.mot ASC;'
-								);
-								try {
-									$bdd->beginTransaction();
-									$lieu_motcles_choice_stmt->execute(array($idlieu));
-									$bdd->commit();
-								} catch (PDOException $e) {
-									$bdd->rollback();
-									echo '<p>'.$e->getMessage().'</p>';
-								}
-								$lieu_motcles_choice_fetch = $lieu_motcles_choice_stmt->fetchAll(PDO::FETCH_ASSOC);
-
-								foreach($lieu_motcles_choice_fetch as $row) {
-									?>
-									<option <?php if (!is_null($row['isselected'])) { echo 'selected'; } ?> value="<?php echo $row['idmot']; ?>"><?php echo $row['mot']; ?></option>
-									<?php
-								}
-								?>
+							<?php
+							$lieu_motcles_choice_stmt = $bdd->prepare(
+								'SELECT motcle.mot, motcle.id AS idmot, lieumotcle.id AS isselected'
+								.' FROM motcle LEFT JOIN lieumotcle ON lieumotcle.idmot = motcle.id AND lieumotcle.idlieu = ?'
+								.' ORDER BY motcle.mot ASC;'
+							);
+							$lieu_motcles_choice_stmt->execute(array($idlieu));
+							$lieu_motcles_choice_fetch = $lieu_motcles_choice_stmt->fetchAll(PDO::FETCH_ASSOC);
+							foreach($lieu_motcles_choice_fetch as $row) {
+							?>
+								<option <?php if (!is_null($row['isselected'])) { echo 'selected'; } ?> value="<?php echo $row['idmot']; ?>"><?php echo $row['mot']; ?></option>
+							<?php
+							}
+							?>
 							</select>
 							<button type="submit" class='btn btn-success mt-1' name="save">Enregistrer</button>
 							<a role="button" onClick="history.go(-1);" class='btn btn-danger mt-1' style="color: white;">Annuler</a>
@@ -77,6 +63,7 @@ if(!is_numeric($idlieu)) {
 			</div>
 			<?php
 		}
+		$bdd->commit();
 	} catch (PDOException $e) {
 		$bdd->rollback();
 		echo '<p>'.$e->getMessage().'</p>';
